f39657e44247145931a256410af4b0b733cf59d6
c.moeller <c.moeller@boservice.de>
2025-11-13 10:48:49 +0100

    added tailwind via npm make it run succesfully

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <p class="bg-red-400">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, ratione.
+    Sequi et totam obcaecati tempore perferendis modi, rerum animi repudiandae?
+  </p>
+</template>
 
 <style scoped></style>
