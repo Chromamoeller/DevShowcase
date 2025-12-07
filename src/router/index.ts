b38ac7d975cd96bb99ba9dfccbf93a67ca10bbe2
Chrism <der.chris.moeller@gmail.com>
2025-12-07 21:47:31 +0100

    sdf

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Herbalism from "../views/Games/WordOfWarcraftHerbalism/Herbalism.vue";
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "home",
     component: Home,
+  },
+  {
+    path: "/games/word-of-warcraft-herbalism",
+    name: "word-of-warcraft-herbalism",
+    component: Herbalism,
   },
 ];
 export const router = createRouter({ history: createWebHistory(), routes });
